b228f0841f2a8835e69c66d26b484bc4b6f9fe0f
Aitor Iturrioz <riturrioz@gmail.com>
2020-03-10 16:23:37 +0100

    Publish multiple devices information
    Signed-off-by: Aitor Iturrioz <riturrioz@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,31 +1,27 @@
 #!/usr/bin/env python
 
-import ipaddress
 import logging
 import signal
-from zeroconf import ServiceInfo, Zeroconf
+from iombian_advertiser import IoMBianAdvertiser
+from people import people
 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(name)-20s  - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+devices = []
+
 def signal_handler(signal, frame):
     logger.info("Stopping 'iombian-devices-simulator'...")
-    zeroconf.unregister_service(service_info)
-    zeroconf.close()
+    for device in devices:
+        device.stop()
 
 if __name__ == "__main__":
     logger.info("Starting 'iombian-devices-simulator'...")
 
-    service_info = ServiceInfo(
-        type_="_iombian._tcp.local.",
-        name="iombian-jhernandez._iombian._tcp.local.",
-        addresses=[ipaddress.ip_address("192.168.10.100").packed],
-        port=80,
-        properties={"Node-RED":1880, "MQTT-client":1888, "Monit":2812},
-        server="iombian-jhernandez.local.")
-
-    zeroconf = Zeroconf()
-    zeroconf.register_service(service_info)
+    for person in people:
+        d = IoMBianAdvertiser(person.get("name"), person.get("last_name"))
+        devices.append(d)
+        d.start()
 
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
